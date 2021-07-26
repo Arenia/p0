4d5142819ba5a1d07ee92c21c8b52fee9190ea07
Arenia <tyler.bagala@gmail.com>
2021-07-26 18:31:38 -0400

    Nearly done, hung on "not a statement"

--- src/main/java/DBCrafter.java
+++ src/main/java/DBCrafter.java
@@ -1,43 +1,88 @@
+import com.opencsv.CSVReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
-import com.opencsv.CSVReader;
 
 //TODO: Construct with knowledge of maven, reliant on SQLite packages as well as OpenCSV
 
 public class DBCrafter {
     public static void main(String[] args){
-        /*
-        if(args.length >= 1){
-            try (CSVReader reader = new CSVReader(new FileReader("file.csv"))) {
-                List<String[]> r = reader.readAll();
-                r.forEach(x -> System.out.println(Arrays.toString(x)));
-            }
-            catch(FileNotFoundException ex){
-                System.out.println("File does not exist, check your args and try again.");
-            }
-            catch(Exception ex){
-                System.err.println("Error: " + ex.getMessage());
-            }
+        List<String[]> data = readCSV(args[0]);
+        if (data != null) {
+            saveDB(data);
         }
-        else {
-            System.out.println("Usage: java DBCrafter (file to read)");
+        else{
+            System.out.println("Bad read?");
         }
-         */
-        readCSV(args[0]);
     }
 
-    public static void readCSV(String filename){
+    public static List<String[]> readCSV(String filename){
         try (CSVReader reader = new CSVReader(new FileReader(filename))) {
-            List<String[]> r = reader.readAll();
-            r.forEach(x -> System.out.println(Arrays.toString(x)));
+            return reader.readAll();
         }
         catch(FileNotFoundException ex){
             System.out.println("File does not exist, check your call and try again.");
         }
         catch(Exception ex){
             System.err.println("Error: " + ex.getMessage());
+        }
+        return null;
+    }
+
+    private static void saveDB(List<String[]> data){
+        Connection connection = null;
+        try{
+
+            //create connection
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30); //Sets timeout to 30 seconds
+
+            statement.executeUpdate("drop table if exists pokemon");
+            statement.executeUpdate("create table pokemon (int id, string name, string type1, string type2, int hp, int attack, int defense, int sp_attack, int sp_defense, int speed)");
+
+            //Use StringBuilder to conform data to table insertion
+            StringBuilder sb = new StringBuilder();
+            sb.append("values(");
+            data.forEach( x-> {
+                System.out.println(Arrays.toString(x));
+                //flush sb each pass
+                sb.delete(0, sb.length()-1);
+                sb.append("values(");
+                for( String s : x ){
+                    sb.append(s).append(", ");
+                }
+                sb.replace((sb.length()-3), sb.length()-1, ")");
+                try {
+                    (statement.executeUpdate("insert into pokemon "+ sb));
+                } catch (SQLException ex) {
+                    System.err.println(ex.getMessage());
+                }
+
+            });
+
+
+        }
+        catch(SQLException ex){
+            System.err.println(ex.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException ex)
+            {
+                // connection close failed.
+                System.err.println(ex.getMessage());
+            }
         }
     }
 }
--- src/main/java/Pokeavg.java
+++ src/main/java/Pokeavg.java
@@ -1,48 +1,25 @@
-/*
-import java.io.File;
+import com.opencsv.CSVReader;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-*/
-import java.util.Scanner;
+import java.io.FileReader;
+import java.util.List;
 
 
 public class Pokeavg {
     public static void main (String[] args) {
         //specify a database to retrieve information from
-        /*
-        if( args.length >= 2 ) {
-            File data = new File(args[0]);
-
-            //retrieve data from specified DB
-            Map<Integer, String> mons = new HashMap<>();
-            int count_id = 1;
-            try(Scanner s = new Scanner(data)){
-                while(s.hasNext()){
-                    String name = s.next();
-                    System.out.println("Checking line " + count_id);
-                    mons.put(count_id, name);
-                    count_id+=1;
+        try (CSVReader reader = new CSVReader(new FileReader(args[0]))) {
+            List<String[]> r = reader.readAll();
+            r.forEach(x -> {
+                for (String s : x) {
+                    System.out.println(s);
                 }
-            }
-            catch (FileNotFoundException ex) {
-                System.out.println("Database file not found. Check path and retry.");
-            }
-
-            //Return name from ID given
-            System.out.println(mons.get(Integer.valueOf(args[1])));
+            });
         }
-        else {
-            System.out.println("Usage: specify the path of the data file then the ID you wish to give, for example:\n java PokeAVG og151.txt 1");
+        catch(FileNotFoundException ex){
+            System.out.println("File does not exist, check your call and try again.");
         }
-         */
-        if(args.length > 1){
-            DBCrafter.readCSV(args[0]);
-        }
-        else{
-            Scanner sysin = new Scanner(System.in);
-            System.out.print("Specify the filename to use: ");
-            DBCrafter.readCSV(sysin.nextLine());
+        catch(Exception ex){
+            System.err.println("Error: " + ex.getMessage());
         }
     }
 
