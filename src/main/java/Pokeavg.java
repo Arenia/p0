1908b700ec1c627c890bf54ef54c47dac3d3daa2
Arenia <tyler.bagala@gmail.com>
2021-07-28 17:38:04 -0400

    Cleanup basic search function

--- src/main/java/Pokeavg.java
+++ src/main/java/Pokeavg.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
 import java.sql.*;
 import java.util.List;
@@ -9,20 +11,14 @@ public class Pokeavg {
     public static void main (String[] args) {
         //Check if db exists, if not populate
         DBCheck(args);
-        if (args.length > 1) {
-            getSingleData(args[1]);
-        } else {
-            Scanner read_in = new Scanner(System.in);
-            System.out.print("Specify a pokemon id to look up: ");
-            getSingleData(read_in.nextLine());
-        }
+        getSingleData();
     }
 
     private static void DBCheck(String[] args){
         List<String[]> data;
         if (new File("pokemon.db").isFile()) {
             Scanner read_in = new Scanner(System.in);
-            System.out.print("Repopulate DB? (Y/N)");
+            System.out.print("Repopulate DB? (Y/N) ");
             String result = read_in.nextLine().toUpperCase();
             if(result.equals("Y")){
                 if (args.length > 0) {
@@ -48,9 +44,14 @@ public class Pokeavg {
         }
     }
 
-    private static void getSingleData(String name) {
-        //Confirm name is capitalized
-        name = name.substring(0,1).toUpperCase() + name.substring(1);
+    private static void getSingleData() {
+        Scanner read_in = new Scanner(System.in);
+        System.out.println("Give me a pokemon name or dex number to look up: ");
+        String search_term = read_in.nextLine();
+        if(!StringUtils.isNumeric(search_term)) {
+            //Confirm name is capitalized
+            search_term = search_term.substring(0, 1).toUpperCase() + search_term.substring(1);
+        }
         Connection connection;
         ResultSet rs;
         try {
@@ -58,7 +59,48 @@ public class Pokeavg {
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30); //Sets timeout to 30 seconds
 
-            rs = statement.executeQuery("select * from pokemon where name='" + name+"'");
+            if(StringUtils.isNumeric(search_term)){
+                rs = statement.executeQuery("select * from pokemon where id=" + search_term);
+            }
+            else{
+                rs = statement.executeQuery("select * from pokemon where name='" + search_term+"'");
+            }
+            System.out.println("Pokedex ID number: " + rs.getInt("id"));
+            System.out.println("Pokemon name: " + rs.getString("name"));
+            System.out.println("Type 1: " + rs.getString("type1"));
+            System.out.println("Type 2: " + rs.getString("type2"));
+            System.out.println("HP: " + rs.getInt("hp"));
+            System.out.println("Attack: " + rs.getInt("attack"));
+            System.out.println("Defense: " + rs.getInt("defense"));
+            System.out.println("Special Attack: " + rs.getInt("sp_attack"));
+            System.out.println("Special Defense: " + rs.getInt("sp_defense"));
+            System.out.println("Speed: " + rs.getInt("speed"));
+            int bst = rs.getInt("hp") + rs.getInt("attack") + rs.getInt("defense")  + rs.getInt("sp_attack") + rs.getInt("sp_defense") + rs.getInt("speed");
+            System.out.println("Base stat total: " + bst);
+        }
+        catch(SQLException ex){
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    private static void getSingleData(String search_term) {
+        if(!StringUtils.isNumeric(search_term)) {
+            //Confirm name is capitalized
+            search_term = search_term.substring(0, 1).toUpperCase() + search_term.substring(1);
+        }
+        Connection connection;
+        ResultSet rs;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:pokemon.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30); //Sets timeout to 30 seconds
+
+            if(StringUtils.isNumeric(search_term)){
+                rs = statement.executeQuery("select * from pokemon where id=" + search_term);
+            }
+            else{
+                rs = statement.executeQuery("select * from pokemon where name='" + search_term+"'");
+            }
             System.out.println("Pokedex ID number: " + rs.getInt("id"));
             System.out.println("Pokemon name: " + rs.getString("name"));
             System.out.println("Type 1: " + rs.getString("type1"));
